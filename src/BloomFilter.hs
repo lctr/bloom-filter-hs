7bbe9834340c71ec33c1b76255bb1a9aa7431374
Lictor Guzman <lctrgzmn@gmail.com>
2022-08-30 01:50:13 -0500

    specified export list

--- src/BloomFilter.hs
+++ src/BloomFilter.hs
@@ -1,11 +1,17 @@
-module BloomFilter where
+module BloomFilter (
+  Bloom
+  , length
+  , elem
+  , notElem
+  , fromList
+) where
 
-import BloomFilter.Internal
-import BloomFilter.Mutable (insert, new)
-import Data.Array.IArray (bounds, (!))
-import Data.Array.ST (runSTUArray)
-import Data.Word (Word32)
-import Prelude hiding (elem, length, notElem)
+import           BloomFilter.Internal
+import           BloomFilter.Mutable  (insert, new)
+import           Data.Array.IArray    (bounds, (!))
+import           Data.Array.ST        (runSTUArray)
+import           Data.Word            (Word32)
+import           Prelude              hiding (elem, length, notElem)
 
 length :: Bloom a -> Int
 length = fromIntegral . len
@@ -21,3 +27,15 @@ elt `elem` fltr = all test (blmHash fltr elt)
 notElem :: a -> Bloom a -> Bool
 elt `notElem` fltr = not (elt `elem` fltr)
 
+-- | An ergonomic way to create an /immutable/ Bloom filter from a list of hash
+-- functions, a capacity, and a list of items to store.
+fromList
+  :: (a -> [Word32]) -- ^ family of hash functions to use
+  -> Word32          -- ^ number of bits in the filter
+  -> [a]             -- ^ values with which to populate the filter
+  -> Bloom a
+fromList hash numBits values =
+  B hash $ runSTUArray $
+    do mb <- new hash numBits
+       mapM_ (insert mb) values
+       return (mutArray mb)
